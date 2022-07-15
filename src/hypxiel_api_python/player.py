41ecf120b192dea85889bf31136e593c3331b7f8
HoggyTheWizard <hogwartswizrd@icloud.com>
2022-07-14 22:58:50 -0400

    Fix args in get function declarations

--- src/hypxiel_api_python/player.py
+++ src/hypxiel_api_python/player.py
@@ -7,14 +7,14 @@ class HypixelPlayer:
         self.hypixel_api = hypixel_api
 
     # support for names needs to be added
-    async def network_level(self, uuid):
-        player = await get(self, endpoint="player", param=("uuid", uuid))
+    async def network_level(self, uuid: str):
+        player = await get(hypixel_api=self.hypixel_api, endpoint="player", params={"uuid": uuid})
         network_experience = player["player"]["networkExp"] if "networkExp" in player["player"] else 0
         network_level = (sqrt((2 * network_experience) + 30625) / 50) - 2.5 if network_experience != 0 else 1
         return network_level
 
-    async def rank(self, uuid):
-        player = await get(self, endpoint="player", param=("uuid", uuid))
+    async def rank(self, uuid: str):
+        player = await get(self.hypixel_api, endpoint="player", params={"uuid", uuid})
         if "rank" in player["player"] and not player["player"]["rank"] == "NORMAL":
             return player["player"]["rank"]
 
