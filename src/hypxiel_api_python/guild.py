12ba559b2f3849f1ec96e753c6642cb25f23a739
HoggyTheWizard <hogwartswizrd@icloud.com>
2022-07-15 19:42:28 -0400

    Create wrapper for /guild

---
+++ src/hypxiel_api_python/guild.py
@@ -0,0 +1,90 @@
+from src.hypxiel_api_python.utils.requests import get
+from src.hypxiel_api_python.hypixel import HypixelAPI
+
+# SUPPORT FOR NAMES NEEDS TO BE ADDED.
+
+
+class HypixelGuild:
+    def __init__(
+            self,
+            hypixel_api: HypixelAPI,
+            params: dict[str: str] = {}
+    ):
+        self.guild = await get(hypixel_api=hypixel_api, endpoint="guild", params=params)["guild"]
+
+    async def raw(self) -> dict:
+        return self.guild
+
+    async def info(self):
+        return {
+            "name": self.guild["name"],
+            "description": self.guild.get("description"),
+            "id": self.guild["_id"],
+            "created": self.guild["created"],
+        }
+
+    async def members(self):
+        return HypixelGuildMembers(hypixel_guild=self.guild)
+
+    async def ranks(self):
+        return self.guild["ranks"]
+
+    async def game_gxp(self):
+        return self.guild["guildExpByGameType"]
+
+    async def tag(self):
+        return {
+            "name": self.guild.get("tag"),
+            "color": self.guild.get("tagColor")
+        }
+
+    async def settings(self):
+        return {
+            "joinable": self.guild.get("joinable"),
+            "publiclyListed": self.guild.get("publiclyListed"),
+        }
+
+    async def legacy(self):
+        return {
+            "legacyRanking": self.guild.get("legacyRanking"),
+            "coins": self.guild.get("coins", 0),
+            "coinsEver": self.guild.get("coinsEver", 0),
+        }
+
+    async def achievements(self):
+        return self.guild["achievements"]
+
+
+class HypixelGuildMembers:
+    def __init__(
+            self,
+            hypixel_guild: HypixelGuild,
+    ):
+        self.members = hypixel_guild.guild["members"]
+
+    async def raw(self) -> list:
+        return self.members
+
+    async def order_by_daily_exp(self):
+        return sorted(
+            [member for member in self.members],
+            key=lambda member: list(member["expHistory"])[0],
+            reverse=True
+        )
+
+    async def order_by_weekly_exp(self):
+        return sorted(
+            [member for member in self.members],
+            key=lambda member: sum(list(member["expHistory"])),
+            reverse=True
+        )
+
+    async def order_by_quest_participation(self):
+        return sorted(
+            [member for member in self.members],
+            key=lambda member: member["questParticipation"],
+            reverse=True
+        )
+
+    async def members_with_guild_rank(self, guild_rank: str):
+        return [member for member in self.members if member["rank"] == guild_rank]
